c45b6195059b3f53afcb6b2f3a168dffa4cad39b
soumyabratakole <soumyabrata.kole@gmail.com>
2021-10-23 01:31:15 -0700

    first-cut

---
+++ src/main/scala/rdd/airports/AirportsByLatitudeSolution.scala
@@ -0,0 +1,27 @@
+package rdd.airports
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.DecimalType
+
+object AirportsByLatitudeSolution {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkSession = SparkSession.builder()
+      .master("local[2]").appName("AirportLatitude")
+      .getOrCreate()
+
+    val input = sparkSession.read.csv("in/airports.text")
+    val castDF = input
+      .withColumn("latitude", col("_c6").cast(new DecimalType(38, 2)))
+      .drop(col("_c6"))
+      .filter(col("latitude") > 40)
+      .select(col("_c1"), col("latitude"))
+    castDF.write
+      .mode("overwrite")
+      .csv("out/airports_in_usa.text")
+
+  }
+
+}
